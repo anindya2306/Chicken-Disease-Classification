1e389f67d588dee58cb888ab763968b3fa4625db
Anindya <joyanindya99@gmail.com>
2023-06-28 19:08:54 +0530

    Base model is prepared

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -12,5 +12,23 @@ class DataIngestionConfig:
     unzip_dir: Path
 
 
+# Entity for basemodel creation
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+
+
 
       
